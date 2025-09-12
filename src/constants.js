72a289cdc8fc69e69289f1acbb15942af8e9bcd0
SHIVAM BARANWAL <98750358+Shivambaranwal267@users.noreply.github.com>
2025-09-12 15:27:00 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,200 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import androidStudioLogo from "./assets/tech_logo/androidStudio.png";
+import flutterLogo from "./assets/tech_logo/flutter.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import laravelLogo from "./assets/tech_logo/laravel.png";
+import dartLogo from "./assets/tech_logo/dart.png";
+
+// Experience Section Logo's
+import webverseLogo from "./assets/company_logo/webverse_logo.png";
+import agcLogo from "./assets/company_logo/agc_logo.png";
+import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+
+// Education Section Logo's
+import glaLogo from "./assets/education_logo/gla_logo.png";
+import bsaLogo from "./assets/education_logo/bsa_logo.png";
+import TmvLogo from "./assets/education_logo/tmvlogo.jpg";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import fssamuLogo from "./assets/work_logo/fssamu.png";
+import EcommerceLogo from "./assets/work_logo/ecommerce.png";
+import weatherLogo from "./assets/work_logo/weather.jpg";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "SASS", logo: sassLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Flutter", logo: flutterLogo },
+      { name: "Dart", logo: dartLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap 5", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "Laravel", logo: laravelLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      { name: "Dart" },
+
+      { name: "JavaScript", logo: javascriptLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Android Studio", logo: androidStudioLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: webverseLogo,
+    role: "Fullstack Developer, Flutter Developer",
+    company: "Cineguru Pvt Ltd",
+    date: "Dec 2024 - May 2025",
+    desc: "Developed dynamic and scalable web applications and android development using the Full stack and full stack android, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    skills: [
+      "HTML",
+      "CSS",
+      "SASS",
+      "JavaScript",
+      "React JS",
+      "Express JS",
+      "Node JS",
+      "Bootstarp 5",
+      "Tailwind CSS",
+      "MongoDb",
+      "Laravel",
+      "Php",
+      "Flutter",
+      "dart",
+      "Hostinger",
+      "MySQL",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img:TmvLogo,
+    school: "Saraf College, Mumbai",
+    date: "Sept 2021 - Aug 2024",
+    grade: "73.2%",
+    desc: "I completed my Bachelor's degree in Computer Science (BCA) from Saraf College, Maumbai. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Bachelor of Science - BSC (Computer Science)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "FSSAMU",
+    description:
+      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    image: fssamuLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github:
+      "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    webapp: "https://fssamu.in",
+    playstore:
+      "https://play.google.com/store/apps/details?id=com.fssamu&hl=en_IN&pli=1",
+  },
+  {
+    id: 1,
+    title: "Ecommerce Website ",
+    description:
+      "Clothing Blue Ten is a sleek e-commerce platform showcasing modern fashion, built with Next.js and deployed on Vercel for optimal performance. It features a responsive, user-friendly interface with a dynamic product catalog and secure checkout. The minimalist design emphasizes accessibility and engagement, catering to style-conscious shoppers across devices",
+    image: EcommerceLogo,
+    tags: [
+      "React JS",
+      "Laravel",
+      "MySQL",
+      "SASS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "PHP",
+    ],
+    github: "https://github.com/Shivambaranwal267/clothing",
+    webapp: "https://clothing-blue-ten.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    description:
+      "Weather App is a streamlined application developed to provide real-time weather updates and forecasts for users worldwide. Built with modern web technologies, it features an intuitive interface displaying key weather metrics, such as temperature, humidity, and wind speed. Hosted on GitHub, the project showcases proficiency in API integration and responsive design for seamless cross-device functionality",
+    image: weatherLogo,
+    tags: ["Java", "API", "Android Studio"],
+    github: "https://github.com/Shivambaranwal267/Weather-App",
+  },
+];
